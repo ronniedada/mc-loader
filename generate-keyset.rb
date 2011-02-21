1e90de988927f3482507a36a8af125627c113dd3
Aliaksey Kandratsenka <alk@tut.by>
2011-02-21 10:49:51 -0800

    some options for generate-keyset.rb and massive speedup

--- generate-keyset.rb
+++ generate-keyset.rb
@@ -1,9 +1,29 @@
 #!/usr/bin/ruby
 
+require 'rubygems'
+require 'trollop'
 
-raise "Need size argument" unless ARGV[0]
-size = ARGV[0].to_i
+$opts = Trollop::options do
+  opt :keys, "keyset size", :type => :int, :required => true
+  opt :value_size, "size of values", :type => :int, :default => 1024
+end
 
-(0...size).each do |i|
-  print("%08d a\n" % [i])
+def generate_value
+  @value ||= begin
+               "a" * $opts[:value_size]
+             end
+end
+
+# value = generate_value()
+
+# (0...$opts[:keys]).each do |i|
+#   print("%08d %s\n" % [i, value])
+# end
+
+i = $opts[:keys]-1
+
+suffix = " #{generate_value}\n"
+while i >= 0
+  STDOUT << sprintf("%08d", i) << suffix
+  i -= 1
 end
